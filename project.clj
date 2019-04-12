81ac12305ad09903bd2c6853a6fb18d06f34c137
Bill Hedworth <bill.hedworth@ovoenergy.com>
2019-04-12 09:20:23 +0100

    kafka 2.2.0

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [org.apache.kafka/kafka-streams "2.0.0"]
-                 [org.apache.kafka/kafka-clients "2.0.0"]
-                 [org.apache.kafka/kafka-streams-test-utils "2.0.0"]
+                 [org.apache.kafka/kafka-streams "2.2.0"]
+                 [org.apache.kafka/kafka-clients "2.2.0"]
+                 [org.apache.kafka/kafka-streams-test-utils "2.2.0"]
                  [org.apache.kafka/kafka-clients "1.1.0" :classifier "test"]
                  [org.clojure/tools.logging "0.4.0"]
                  [org.slf4j/slf4j-log4j12 "1.7.1"]
